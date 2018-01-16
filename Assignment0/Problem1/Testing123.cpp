cfa0018c7f5e2b708aeeb53eb67aa47f91b4c804
Brian <Brian@BRIAN-YOGA>
2018-01-16 12:26:13 -0500

    Assignment0

---
+++ Assignment0/Problem1/Testing123.cpp
@@ -0,0 +1,24 @@
+//
+//	Brian Manley					CPSC 200				
+//	manleyb2@ferris.edu				Spring 2018
+//									Assignment 0
+//	FILE: Testing123.cpp			1/11/2018
+//
+
+//Oath of Originality: I, Brian Manley, pledge that the
+//contents of this file are my own independent work and conform to
+//all University policies on academic integrity.
+
+// Testing the Visual C++ compiler
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Testing 1, 2, 3." << endl;
+
+	printf_s("Hello World \n");
+	
+	return;
+}
