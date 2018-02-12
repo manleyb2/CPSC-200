5e25c07a9e43f3b780b3c63515c98b6b669fee7d
Brian <Brian@BRIAN-GAME>
2018-02-11 22:51:13 -0500

    Completed Assignment 3

--- Assignment 3/Problem 1/Source.cpp
+++ Assignment 3/Problem 1/Source.cpp
@@ -1,10 +1,81 @@
+/*
 
-#include <
+	Brian Manley					CPSC 200
+	manleyb2@ferris.edu				Spring 2018
+	Problem 1						Assignment 3
+	FILE: Source.cpp				2/11/2018
 
-bool isPerfect(int number)
+
+Oath of Originality: I, Brian Manley, pledge that the 
+contents of this file are my own independent work and conform to
+all University policies on academic integrity.
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+bool isPerfect(int number);
+void isPerfectPrint(int number);
 
 void main()
 {
+	for (int i = 1; i <= 1000; i++)
+	{
+		bool perfect;
+
+		perfect = isPerfect(i);				//If i is a perfect number "perfect is true
+
+		if (perfect == true)
+		{
+			isPerfectPrint(i);				//Run same function but with print statements
+			cout << "	" << i << endl;		//Print perfect number
+		}
 
 
+	}
+
+}
+
+bool isPerfect(int number)
+{
+	float test;
+	int i, total = 0;
+
+	
+
+	for (i = 1; i < number; i++)		//itertate from 1 to the tested number
+	{
+
+		if (number%i == 0)				//test if i is a factor
+		{
+			total += i;					//add all factors together
+		}
+	}
+		
+	if (total == number)				//if total equals the number then return true, otherwise false
+		return true;
+	else
+		return false;
+}
+
+
+void isPerfectPrint(int number)			//Run same as last funtion but since number is already known to be perfect print each factor
+{
+	float test;
+	int i, total = 0;
+
+	
+
+	for (i = 1; i < number; i++)
+	{
+
+		if (number%i == 0)
+		{
+			cout << i << endl;
+		}
+	}
+		
+	return;
 }
---
+++ Assignment 3/Problem 2/Source.cpp
@@ -0,0 +1,64 @@
+﻿/*
+
+Brian Manley					CPSC 200
+manleyb2@ferris.edu				Spring 2018
+Problem 2						Assignment 3
+FILE: Source.cpp				2/11/2018
+
+
+Oath of Originality: I, Brian Manley, pledge that the
+contents of this file are my own independent work and conform to
+all University policies on academic integrity.
+
+*/
+
+
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+double MySine(double x);
+int machine();
+
+void main()
+{
+	double input, cResult, result;
+	cout << "Press ctrl + c to quit at anytime.";
+	while (1 == 1)															//run forever
+	{
+		cout << "Enter a number between -(pi)/2 and (pi)/2: ";				//recieve input
+		cin >> input;
+		cout << endl;
+
+		if (input <= M_PI_2)												// test if input is within standards
+		{																	// if it does run the sin calls and display them
+			cResult = MySine(input);
+			result = sin(input);
+			cout << "Calculated MySin is " << cResult << endl;
+			cout << "Calculated sin is   " << result << endl << endl;
+		}
+			
+
+
+	}
+
+}
+
+double MySine(double x)
+{
+
+	double limit, sum, test;
+	test = x;
+	sum = x;									
+	for (int i = 1;; i++)
+	{	
+		test = (test*(-1)*x*x) / (2 * i*(2 * i + 1));		//reduced power series to Tn=Tn−1x2(2n+1)2n
+		if (test == 0)										//Test will eventually hit a number lower than double will handle, so end for loop
+			break;
+		sum = sum + test;									//adding the numbers together will find the sin of x
+	}
+
+	return sum;
+}
